ede5540360bacb3c8333e74e3a8c3064ce6efedf
서주희 <maywngml@naver.com>
2025-03-23 23:16:20 +0900

    [feat] 라우팅 구현, a 태그 클릭 기능 구현, popstate 이벤트리스너 구현

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import { HomePage, ProfilePage, LoginPage, NotFoundPage } from "./pages";
 
-document.body.innerHTML = `
-  ${HomePage()}
-  ${ProfilePage()}
-  ${LoginPage()}
-  ${NotFoundPage()}
-`;
+const App = () => {
+  if (location.pathname === "/") {
+    return HomePage();
+  } else if (location.pathname === "/login") {
+    return LoginPage();
+  } else if (location.pathname === "/profile") {
+    return ProfilePage();
+  } else {
+    return NotFoundPage();
+  }
+};
+
+const render = () => {
+  document.body.innerHTML = App();
+
+  document.querySelectorAll("a").forEach((el) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      const newPathName = e.target.href.replace(location.origin, "");
+      history.pushState(null, "", newPathName);
+      render();
+    });
+  });
+};
+
+window.addEventListener("popstate", () => {
+  render();
+});
+
+render();
