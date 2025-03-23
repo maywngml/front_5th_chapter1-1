4030df649c683335951e066b448948f0a94df140
서주희 <maywngml@naver.com>
2025-03-23 20:45:49 +0900

    [refactor] 각 페이지들 모듈화

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as ErrorPage } from "./ErrorPage";
+export { default as LoginPage } from "./LoginPage";
+export { default as MainPage } from "./MainPage";
+export { default as ProfilePage } from "./ProfilePage";
