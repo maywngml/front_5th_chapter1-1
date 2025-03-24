71777a9116d760d3623b9e8f0ec9aafa92b99f2b
서주희 <maywngml@naver.com>
2025-03-24 22:20:12 +0900

    [feat] user 타입 추가, 사용자 정보 가져오는 함수 추가

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,10 +1,22 @@
-export function login(username: string) {
+import type { User } from "@/types/user";
+
+export const login = (username: string) => {
   localStorage.setItem(
     "user",
     JSON.stringify({ username, email: "", bio: "" }),
   );
-}
+};
 
-export function logout() {
+export const logout = () => {
   localStorage.removeItem("user");
-}
+};
+
+export const getUser = (): User | null => {
+  const userValue = localStorage.getItem("user");
+  let parsedUser = null;
+
+  if (userValue) {
+    parsedUser = JSON.parse(userValue);
+  }
+  return parsedUser;
+};
