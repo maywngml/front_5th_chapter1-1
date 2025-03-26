05fb98407ea2621cc7e2236b7d5dea616a506094
서주희 <maywngml@naver.com>
2025-03-27 03:16:49 +0900

    [chore] gh-pages 배포 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+import { resolve } from "path";
 import { defineConfig } from "vitest/config";
 
+const isCI = process.env.CI === "true";
+
 export default defineConfig({
+  base: isCI ? "/" : "/front_5th_chapter1-1/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        hash: resolve(__dirname, "index.hash.html"),
+      },
+    },
+  },
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
